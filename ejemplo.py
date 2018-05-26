b46ec3cc26290da152ecf16838e8caa4db93abdd
unknown <claudio1624>
2018-05-26 12:52:09 -0400

    primer comit

---
+++ ejemplo.py
@@ -0,0 +1,8 @@
+import os, sys
+a = int(input("Primer Numero :"))
+b = int(input("Segundo numero:"))
+c = int(input("tercer Nemero:"))
+d = int(input("Cuarto numero:"))
+suma = a+b+c+d
+resultado='la sumatoria es'
+print(resultado, suma)
---
+++ suma.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+# -*- coding: iso-8859-15 -*-
+import os, sys
+a = int(input("Unidades :"))
+b = int(input("Precio :"))
+print("A pagar=", a*b)
