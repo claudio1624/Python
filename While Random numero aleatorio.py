941ce54502e7f44bc094b5c7cbc50bea3c963d7a
unknown <claudio1624>
2018-07-20 13:21:52 -0400

    numero aleatorios

---
+++ While Random numero aleatorio.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+from random import *
+x = 0
+while x != 3:
+    x = randint (1,6)
+    print'El numero aleatorio es ', x
+
+print''
+print'FIN 3 ENCNTRADO'  
+print''
